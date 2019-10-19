a92da328a508959d5e78703360bff2369cdd697a
Luis <luisslf.1@gmail.com>
2019-10-19 08:17:27 -0300

    First commit

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Aprendendo React
         </a>
       </header>
     </div>
